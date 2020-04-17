dc7e7723c825222b0d3b38f6502edc06538a12e2
root <root@DESKTOP-2KLJLBF.localdomain>
2020-04-17 16:32:13 -0400

    Changed kiln temperature to be a pot property.

--- scripts/HairyPotterPart1.js
+++ scripts/HairyPotterPart1.js
@@ -1,23 +1,23 @@
 let pots = []
 // Function that creates object representations of pots
 let potId = 1
-const potteryFactory = (id, shape, weight, height) => {
+const potteryFactory = (id, shape, weight, height, kilnTemp) => {
   return {
     id,
     shape,
     weight,
     height,
+    kilnTemp
   }
 }
 
-const pot1 = potteryFactory(potId++, "bowl", 2, 20);
-const pot2 = potteryFactory(potId++, "urn", 5, 20);
-const pot3 = potteryFactory(potId++, "urn", 6, 20);
-const pot4 = potteryFactory(potId++, "jar", 3, 20);
-const pot5 = potteryFactory(potId++, "vase", 4, 20);
+const pot1 = potteryFactory(potId++, "bowl", 2, 20, 2000);
+const pot2 = potteryFactory(potId++, "urn", 5, 20, 2200);
+const pot3 = potteryFactory(potId++, "urn", 6, 20, 2250);
+const pot4 = potteryFactory(potId++, "jar", 3, 20, 2300);
+const pot5 = potteryFactory(potId++, "vase", 4, 20, 1950);
 
 pots.push(pot1, pot2, pot3, pot4, pot5)
-console.log(pots)
 
 // Function that runs pots through the kiln
 const kiln = (sinlglePot, kilnTemp) => {
@@ -34,15 +34,13 @@ const kiln = (sinlglePot, kilnTemp) => {
 
 let firedPots = []
 
-const firedPot1 = kiln(pots[0], 2000)
-const firedPot2 = kiln(pots[1], 2200)
-const firedPot3 = kiln(pots[2], 2250)
-const firedPot4 = kiln(pots[3], 2300)
-const firedPot5 = kiln(pots[4], 1950)
+const firedPot1 = kiln(pots[0], pots[0].kilnTemp)
+const firedPot2 = kiln(pots[1], pots[1].kilnTemp)
+const firedPot3 = kiln(pots[2], pots[2].kilnTemp)
+const firedPot4 = kiln(pots[3], pots[3].kilnTemp)
+const firedPot5 = kiln(pots[4], pots[4].kilnTemp)
 
 firedPots.push(firedPot1, firedPot2, firedPot3, firedPot4, firedPot5)
-console.log(firedPots)
-
 
 // Function that determines if it should be sold and how much pottery is worth 
 let potteryToSell = []
